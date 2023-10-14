d5f00110cc25dd545d7a0bc94641adab0df7fe22
jurecapuder <capuder.j@gmail.com>
2023-10-14 19:27:25 +0200

    Moving page file

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button className="bg-emerald-500 text-white">
-        Delete
-      </Button>
+      <Button className="bg-emerald-500 text-white">Delete</Button>
     </div>
-  )
+  );
 }
