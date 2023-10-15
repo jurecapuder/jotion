71a495448f8dc2a70e1078e4c65e2196f5372e56
jurecapuder <capuder.j@gmail.com>
2023-10-15 18:06:33 +0200

    Updating footer

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { Logo } from "./logo"
 
 export const Footer = () => {
   return (
     <div className="flex items-center w-full p-6 bg-background z-50">
-      Footer
+      <Logo />
     </div>
   )
 }
