b33b52ace679ae464928dcd3d1ad7b9ea25a8193
jurecapuder <capuder.j@gmail.com>
2023-10-15 17:11:14 +0200

    Updating description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Jotion',
-  description: 'Notion Clone',
+  description: 'The connected workspace where better, faster work happens.',
 }
 
 export default function RootLayout({
