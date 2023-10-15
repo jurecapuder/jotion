b32b3e1b6520f98689990943f3e6a49742655405
jurecapuder <capuder.j@gmail.com>
2023-10-15 17:35:15 +0200

    Creating heading component

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1>
+        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
+      </h1>
+    </div>
+  )
+}
