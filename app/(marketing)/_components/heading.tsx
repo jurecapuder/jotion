ad033e2536ff73a6c59f6cbd4dd71403eff777ac
jurecapuder <capuder.j@gmail.com>
2023-10-15 17:39:26 +0200

    Adding button

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { ArrowRight } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
 export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
@@ -11,6 +15,12 @@ export const Heading = () => {
         Jotion is the connected workspace where <br />
         better, faster work happens.
       </h3>
+
+      <Button>
+        Enter Jotion
+
+        <ArrowRight />
+      </Button>
     </div>
   )
 }
