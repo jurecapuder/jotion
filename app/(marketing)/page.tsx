38bf576ff8ed95b28a92c4c469263a58b384aed4
jurecapuder <capuder.j@gmail.com>
2023-10-15 17:28:55 +0200

    Updating marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+const MarketingPage = () => {
   return (
     <div>
       <Button className="bg-emerald-500 text-white">Delete</Button>
     </div>
   );
 }
+
+export default MarketingPage;
