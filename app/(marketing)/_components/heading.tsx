0131b87b9128777ad290b8d1ddc82b1b0d8674d6
jurecapuder <capuder.j@gmail.com>
2023-10-15 17:36:15 +0200

    Adding h1 styling

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -3,7 +3,7 @@
 export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
-      <h1>
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
       </h1>
     </div>
