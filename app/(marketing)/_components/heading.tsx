591020aae41b3c1ca36ff592729017077934cdd6
jurecapuder <capuder.j@gmail.com>
2023-10-15 17:37:16 +0200

    Adding h3 text & styling

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -6,6 +6,11 @@ export const Heading = () => {
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
       </h1>
+
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Jotion is the connected workspace where <br />
+        better, faster work happens.
+      </h3>
     </div>
   )
 }
