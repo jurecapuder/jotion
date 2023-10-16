da726638058857cf80d02354fee3066cff7c4aef
jurecapuder <capuder.j@gmail.com>
2023-10-16 23:13:09 +0200

    Adding mode toggle to navbar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
+import { ModeToggle } from "@/components/mode-toggle"; 
 import { cn } from "@/lib/utils";
 
 import { Logo } from "./logo";
@@ -16,7 +17,7 @@ export const Navbar = () => {
       <Logo />
 
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-        Login
+        <ModeToggle />
       </div>
     </div>
   )
