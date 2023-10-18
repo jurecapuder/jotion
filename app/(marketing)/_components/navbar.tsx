39363f9e04d601e0295182ab45c68735f07f65b8
jurecapuder <capuder.j@gmail.com>
2023-10-18 21:26:08 +0200

    Adding user button prop

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -53,7 +53,9 @@ export const Navbar = () => {
               </Link>
             </Button>
 
-            <UserButton />
+            <UserButton
+              afterSignOutUrl="/"
+            />
           </>
         )}
 
