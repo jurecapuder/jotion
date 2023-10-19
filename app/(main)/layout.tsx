16497bd449d449749bfb09325d4da66fb4a98244
jurecapuder <capuder.j@gmail.com>
2023-10-19 19:37:08 +0200

    Updating main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,23 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
+import { useConvexAuth } from "convex/react";
+
 const MainLayout = ({
   children
 }: {
   children: React.ReactNode
 }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    )
+  }
+
   return (
     <div>
       {children}
