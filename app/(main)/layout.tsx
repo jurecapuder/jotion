98826bb27b502ec084af1745d44842ef88c5d9c6
jurecapuder <capuder.j@gmail.com>
2023-10-19 19:59:02 +0200

    Adding navigation to main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+
+import { Spinner } from "@/components/spinner";
+
+import { Navigation } from "./_components/navigation";
 
 const MainLayout = ({
   children
@@ -25,6 +28,8 @@ const MainLayout = ({
 
   return (
     <div className="h-full flex dark:bg-[#1F1F1F]">
+      <Navigation />
+
       <main className="flex-1 h-full overflow-y-auto">
         {children}
       </main>
