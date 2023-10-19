9679f3dfac7e74385342bd71eee81ee3f7227412
jurecapuder <capuder.j@gmail.com>
2023-10-19 23:26:06 +0200

    Updating navigation

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,7 +6,17 @@ export const Navigation = () => {
       <aside
         className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]"
       >
-        Navigation
+        <div>
+          <p>Action items</p>
+        </div>
+
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+
+        <div
+          className="opacity-0"
+        />
       </aside>
     </>
   )
