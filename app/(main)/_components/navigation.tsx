11da0cb376bf84f86872c563c1ddb041d0861947
jurecapuder <capuder.j@gmail.com>
2023-10-19 23:31:44 +0200

    Adding chevrons left icon and styling

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { ChevronsLeftIcon } from "lucide-react";
+
 export const Navigation = () => {
   return (
     <>
       <aside
         className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]"
       >
+        <div
+          role="button"
+          className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+        >
+          <ChevronsLeftIcon className="h-6 w-6" />
+        </div>
+
         <div>
           <p>Action items</p>
         </div>
