b63ceb593b924a38493f5a180613087c1c5bbc43
jurecapuder <capuder.j@gmail.com>
2023-10-20 14:25:14 +0200

    Assigning reset width

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -96,7 +96,7 @@ export const Navigation = () => {
 
         <div
           onMouseDown={handleMouseDown}
-          onClick={() => {}}
+          onClick={resetWidth}
           className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
         />
       </aside>
