85716050bec4e769a4cde15eed8bb3bddcf2e972
jurecapuder <capuder.j@gmail.com>
2023-10-20 12:38:04 +0200

    Assigning refs to navigation

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,8 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
+import { cn } from "@/lib/utils";
+
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -18,7 +20,12 @@ export const Navigation = () => {
   return (
     <>
       <aside
-        className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]"
+        ref={sidebarRef}
+        className={cn(
+          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0"
+        )}
       >
         <div
           role="button"
