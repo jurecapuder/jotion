8bf5d6f3ff0ba66f7f8b951935cbc488643fc427
jurecapuder <capuder.j@gmail.com>
2023-10-20 12:58:01 +0200

    Assigning handle mouse down

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -57,7 +57,7 @@ export const Navigation = () => {
         </div>
 
         <div
-          onMouseDown={() => {}}
+          onMouseDown={handleMouseDown}
           onClick={() => {}}
           className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
         />
