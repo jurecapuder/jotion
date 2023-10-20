f04e5c902bc569d19f69d032a7740c388397be51
jurecapuder <capuder.j@gmail.com>
2023-10-20 14:28:12 +0200

    Creating collapse function

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -66,6 +66,18 @@ export const Navigation = () => {
     }
   }
 
+  const collapse = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = "0";
+      navbarRef.current.style.setProperty("width", "100%");
+      navbarRef.current.style.setProperty("left", "0");
+      setTimeout(() => setIsResetting(false), 300);
+    }
+  }
+
   return (
     <>
       <aside
