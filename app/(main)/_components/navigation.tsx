de37444333abffc6e4cae6a9417f3ea6f59d1c19
jurecapuder <capuder.j@gmail.com>
2023-10-20 13:54:24 +0200

    Adding handle mouse up function

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -42,6 +42,12 @@ export const Navigation = () => {
     }
   }
 
+  const handleMouseUp = () => {
+    isResizingRef.current = false;
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
+  }
+
   return (
     <>
       <aside
