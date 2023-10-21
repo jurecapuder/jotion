ad638fca70972cd8833ea393f1e5495b03430521
jurecapuder <capuder.j@gmail.com>
2023-10-21 22:31:12 +0200

    Adding main page styling

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,6 +1,6 @@
 const DocumentsPage = () => {
   return (
-    <div>
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
       This is a protected page
     </div>
   )
