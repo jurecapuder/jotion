fa5ae77c3ffb4be741ff1b27351babb7900ae8be
jurecapuder <capuder.j@gmail.com>
2023-10-21 22:32:42 +0200

    Adding image to page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import Image from "next/image";
+
 const DocumentsPage = () => {
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
-      This is a protected page
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
     </div>
   )
 }
