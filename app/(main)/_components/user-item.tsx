5f3fedcf518889aae89e5367fcd35123f5c99dd3
jurecapuder <capuder.j@gmail.com>
2023-10-23 19:46:41 +0200

    Adding user item imports

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -1,5 +1,21 @@
 "use client";
 
+import { ChevronsLeftRight } from "lucide-react";
+
+import {
+  Avatar,
+  AvatarImage
+} from "@/components/ui/avatar";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+
 export const UserItem = () => {
   return (
     <div>
