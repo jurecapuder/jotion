274ad191513e13582f13d2faeda26160a1753422
jurecapuder <capuder.j@gmail.com>
2023-10-23 20:09:41 +0200

    Adding avatar image to content

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -48,6 +48,14 @@ export const UserItem = () => {
           <p className="text-xs font-medium leading-none text-muted-foreground">
             {user?.emailAddresses[0].emailAddress}
           </p>
+
+          <div className="flex items-center gap-x-2">
+            <div className="rounded-md bg-secondary p-1">
+              <Avatar className="h-8 w-8">
+                <AvatarImage src={user?.imageUrl} />
+              </Avatar>
+            </div>
+          </div>
         </div>
       </DropdownMenuContent>
     </DropdownMenu>
