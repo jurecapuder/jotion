322aa4ca1716fd338dfca41b764e68789edb9929
jurecapuder <capuder.j@gmail.com>
2023-10-24 20:53:57 +0200

    Updating icon prop

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -11,7 +11,7 @@ interface ItemProps {
 export const Item = ({
   label,
   onClick,
-  icon,
+  icon: Icon,
 }: ItemProps) => {
   return (
     <div
@@ -20,6 +20,8 @@ export const Item = ({
       style={{ paddingLeft: "12px" }}
       className="group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
     >
+      <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+
       {label}
     </div>
   )
