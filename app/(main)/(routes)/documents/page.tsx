1e41048bf33208226951b188c37756f469cf99ed
jurecapuder <capuder.j@gmail.com>
2023-10-24 20:32:35 +0200

    Creating on click function

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { PlusCircle } from "lucide-react";
 import { useMutation } from "convex/react";
+import { toast } from "sonner";
 
 import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
@@ -12,6 +13,16 @@ const DocumentsPage = () => {
   const { user } = useUser();
 
   const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note."
+    });
+  }
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
