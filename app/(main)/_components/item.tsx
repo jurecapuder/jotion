9ab23ffd8629190ffe637d535c76b87ff265a684
jurecapuder <capuder.j@gmail.com>
2023-10-24 21:10:04 +0200

    Adding more item props

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -2,7 +2,16 @@
 
 import { LucideIcon } from "lucide-react";
 
+import { Id } from "@/convex/_generated/dataModel";
+
 interface ItemProps {
+  id?: Id<"documents">;
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearch?: boolean;
+  level?: number;
+  onExpand?: () => void;
   label: string;
   onClick: () => void;
   icon: LucideIcon;
