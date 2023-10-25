04dfe39636a5758590bd346e1af7fddc82b7c51d
jurecapuder <capuder.j@gmail.com>
2023-10-25 13:17:28 +0200

    Updating document list

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+
 import { Doc, Id } from "@/convex/_generated/dataModel";
 
 interface DocumentListProps {
@@ -12,6 +15,17 @@ export const DocumentList = ({
   parentDocumentId,
   level = 0
 }: DocumentListProps) => {
+  const params = useParams();
+  const router = useRouter();
+  const [expanded, setExpanded] = useState<Record<string, boolean>>({});
+
+  const onExpand = (documentId: string) => {
+    setExpanded(prevExpanded => ({
+      ...prevExpanded,
+      [documentId]: !prevExpanded[documentId]
+    }));
+  }
+
   return (
     <div>
       DocumentList
