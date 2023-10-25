b809919ab089d55b7393e6d10bdef62454d1ac8b
jurecapuder <capuder.j@gmail.com>
2023-10-25 12:39:29 +0200

    Updating item

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -74,7 +74,7 @@ export const Item = ({
 
       {isSearch && (
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">⌘</span>k
+          <span className="text-xs">CTRL</span>K
         </kbd>
       )}
     </div>
