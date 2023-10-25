c97721dc37b6ddbebf32b43d35b85db04ac3b19e
jurecapuder <capuder.j@gmail.com>
2023-10-25 13:06:59 +0200

    Updating item

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -8,6 +8,7 @@ import {
 
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -87,8 +88,11 @@ Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
       style={{
         paddingLeft: level ? `${(level * 12) + 25}px` : "12px"
       }}
+      className="flex gap-x-2 py-[3px]"
     >
+      <Skeleton className="h-4 w-4" />
 
+      <Skeleton className="h-4 w-[30%]" />
     </div>
   )
 }
