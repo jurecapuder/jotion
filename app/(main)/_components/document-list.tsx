04b7b2e502721dd9dd1e331f89f0b17cb7409050
jurecapuder <capuder.j@gmail.com>
2023-10-25 13:14:08 +0200

    Updating document list

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -1,6 +1,17 @@
 "use client";
 
-export const DocumentList = () => {
+import { Doc, Id } from "@/convex/_generated/dataModel";
+
+interface DocumentListProps {
+  parentDocumentId?: Id<"documents">;
+  level?: number;
+  data?: Doc<"documents">[];
+}
+
+export const DocumentList = ({
+  parentDocumentId,
+  level = 0
+}: DocumentListProps) => {
   return (
     <div>
       DocumentList
