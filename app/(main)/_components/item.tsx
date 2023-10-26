e86c99f6675a545b1c8192ff8797ea07a0318248
jurecapuder <capuder.j@gmail.com>
2023-10-26 13:44:39 +0200

    Creating handle expand

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -35,6 +35,13 @@ export const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event?.stopPropagation();
+    onExpand?.();
+  }
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
   return (
