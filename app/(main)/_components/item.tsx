47c984cd978407db96b94e94c97b7f1d305dd895
jurecapuder <capuder.j@gmail.com>
2023-10-26 14:15:18 +0200

    Updating item

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -3,7 +3,8 @@
 import {
   ChevronDown,
   ChevronRight,
-  LucideIcon
+  LucideIcon,
+  Plus
 } from "lucide-react";
 
 import { Id } from "@/convex/_generated/dataModel";
@@ -84,6 +85,16 @@ export const Item = ({
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">CTRL</span>K
         </kbd>
+      )}
+
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
       )}
     </div>
   )
