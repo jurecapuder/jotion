a48028d425de3ac29bd9d82c6206daf3f668f722
jurecapuder <capuder.j@gmail.com>
2023-11-02 21:52:35 +0100

    Updating trash box

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { useQuery, useMutation } from "convex/react";
 
@@ -12,7 +13,11 @@ export const TrashBox = () => {
   const restore = useMutation(api.documents.restore);
   const remove = useMutation(api.documents.remove);
 
-  
+  const [ search, setSearch ] = useState("");
+
+  const filteredDocuments = documents?.filter((document) => {
+    return document.title.toLowerCase().includes(search.toLowerCase());
+  });
 
   return (
     <div>
