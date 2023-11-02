761ed7061fb3dbed15e4e99e22e6d22f54b8126b
jurecapuder <capuder.j@gmail.com>
2023-11-02 21:35:03 +0100

    Updating documents

--- convex/documents.ts
+++ convex/documents.ts
@@ -183,11 +183,11 @@ export const restore = mutation({
       }
     }
 
-    await ctx.db.patch(args.id, options);
+    const document = await ctx.db.patch(args.id, options);
     
     recursiveRestore(args.id);
 
-    return existingDocument;
+    return document;
   }
 });
 
