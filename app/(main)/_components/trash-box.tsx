545e7e0af41dc37dd7102cbb92ab1b021c3875d5
jurecapuder <capuder.j@gmail.com>
2023-11-02 21:40:00 +0100

    Adding more variables

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -8,7 +8,11 @@ import { api } from "@/convex/_generated/api";
 export const TrashBox = () => {
   const router = useRouter();
   const params = useParams();
-  const documents = useQuery(api);
+  const documents = useQuery(api.documents.getTrash);
+  const restore = useMutation(api.documents.restore);
+  const remove = useMutation(api.documents.remove);
+
+  
 
   return (
     <div>
