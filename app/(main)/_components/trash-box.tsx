3f6fdc76611ce85925e4072a67c444f13ca4e016
jurecapuder <capuder.j@gmail.com>
2023-11-02 21:39:10 +0100

    Adding imports

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,6 +1,15 @@
 "use client";
 
+import { useParams, useRouter } from "next/navigation";
+import { useQuery, useMutation } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+
 export const TrashBox = () => {
+  const router = useRouter();
+  const params = useParams();
+  const documents = useQuery(api);
+
   return (
     <div>
       Trash box
