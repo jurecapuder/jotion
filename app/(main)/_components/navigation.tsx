55f87161469818118128dac1015475a97a011f0c
jurecapuder <capuder.j@gmail.com>
2023-11-02 21:37:34 +0100

    Adding trash box to navigation

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -26,6 +26,7 @@ import {
 import { UserItem } from "./user-item";
 import { Item } from "./item";
 import { DocumentList } from "./document-list";
+import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -188,7 +189,7 @@ export const Navigation = () => {
               className="p-0 w-72"
               side={isMobile ? "bottom" : "right"}
             >
-              <p>Trash box</p>
+              <TrashBox />
             </PopoverContent>
           </Popover>
         </div>
