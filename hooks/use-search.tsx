38253d43ce55db098f0f4b4ce9186a2f95ee0442
jurecapuder <capuder.j@gmail.com>
2023-11-06 20:20:20 +0100

    Updating use search

--- hooks/use-search.tsx
+++ hooks/use-search.tsx
@@ -6,3 +6,10 @@ type SearchStore = {
   onClose: () => void;
   toggle: () => void;
 };
+
+export const useSearch = create<SearchStore>((set, get) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+  toggle: () => set({ isOpen: !get().isOpen }),
+}));
