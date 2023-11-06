51e25ebbf61001e820dafce37fa7ac6cefeee2d4
jurecapuder <capuder.j@gmail.com>
2023-11-06 20:30:43 +0100

    Adding more imports

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -5,3 +5,14 @@ import { File } from "lucide-react";
 import { useQuery } from "convex/react";
 import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/clerk-react";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList
+} from "@/components/ui/command";
+import { useSearch } from "@/hooks/use-search";
+import { api } from "@/convex/_generated/api";
