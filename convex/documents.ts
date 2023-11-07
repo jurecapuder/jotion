a7557db3adda69dfc2be8e41f84381cc7f747101
jurecapuder <capuder.j@gmail.com>
2023-11-07 19:11:51 +0100

    Updating get search

--- convex/documents.ts
+++ convex/documents.ts
@@ -227,5 +227,16 @@ export const getSearch = query({
     }
 
     const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user", (q) => q.eq("userId", userId))
+      .filter((q) =>
+        q.eq(q.field("isArchived"), false)
+      )
+      .order("desc")
+      .collect();
+    
+    return documents;
   }
 })
