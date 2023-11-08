3f19fe48a7aae71e2e95492d35cca1cc4e87081e
jurecapuder <capuder.j@gmail.com>
2023-11-08 21:17:39 +0100

    Creating modal provider

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { SettingsModal } from "@/components/modals/settings-modal";
+
+export const ModalProvider = () => {
+  return (
+    <>
+      <SettingsModal />
+    </>
+  )
+}
