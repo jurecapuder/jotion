287051ea0b68eaedbca373aa1926ae08c0b0d7d7
jurecapuder <capuder.j@gmail.com>
2023-11-12 22:12:18 +0100

    Add restore and delete buttons to banner component

--- app/(main)/_components/banner.tsx
+++ app/(main)/_components/banner.tsx
@@ -6,6 +6,7 @@ import { toast } from "sonner";
 
 import { Id } from "@/convex/_generated/dataModel";
 import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
 
 interface BannerProps {
   documentId: Id<"documents">;
@@ -43,8 +44,28 @@ export const Banner = ({
   };
 
   return (
-    <div>
-      Banner
+    <div className="w-full bg-rose-500 text-center text-sm p-2 text-white flex items-center gap-x-2 justify-center">
+      <p>
+        This page is in the Trash.
+      </p>
+
+      <Button
+        size="sm"
+        onClick={onRestore}
+        variant="outline"
+        className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
+      >
+        Restore page
+      </Button>
+
+      <Button
+        size="sm"
+        onClick={onRemove}
+        variant="outline"
+        className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
+      >
+        Delete forever
+      </Button>
     </div>
   )
 }
