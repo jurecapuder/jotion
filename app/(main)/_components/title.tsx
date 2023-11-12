1c09e942b39774c5eaf6bbbd10b2630556673426
jurecapuder <capuder.j@gmail.com>
2023-11-12 21:34:29 +0100

    Add input and button components to Title component

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useState } from "react";
 import { useMutation } from "convex/react";
 
 import { Doc } from "@/convex/_generated/dataModel";
 import { api } from "@/convex/_generated/api";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 interface TitleProps {
   initialData: Doc<"documents">;
@@ -14,9 +17,28 @@ export const Title = ({
 }: TitleProps) => {
   const update = useMutation(api.documents.update);
 
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
-    <div>
-      Title
+    <div className="flex items-center gap-x-1">
+      {!!initialData.icon && <p>{initialData.icon}</p>}
+
+      {isEditing ? (
+        <Input
+          className="h-7 px-2 focus-visible:ring-transparent"
+        />
+      ): (
+        <Button
+          onClick={() => {}}
+          variant="ghost"
+          size="sm"
+          className="font-normal h-auto p-1"
+        >
+          <span>
+            {initialData?.title}
+          </span>
+        </Button>
+      )}
     </div>
   );
 };
