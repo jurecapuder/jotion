4b8592b5b2832572f8734acd8ae78d6627a9c88b
jurecapuder <capuder.j@gmail.com>
2023-11-13 18:19:35 +0100

    Adding dropdownmenu imports

--- app/(main)/_components/menu.tsx
+++ app/(main)/_components/menu.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import { Id } from "@/convex/_generated/dataModel";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator
+} from "@/components/ui/dropdown-menu";
 
 interface MenuProps {
   documentId: Id<"documents">;
