172a8174dae352ae93bde1dc83695ab3c11417f5
jurecapuder <capuder.j@gmail.com>
2023-11-14 22:54:23 +0100

    Add documentId props

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -3,6 +3,13 @@
 import { useQuery } from "convex/react";
 
 import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+};
 
 const DocumentIdPage = () => {
   return (
