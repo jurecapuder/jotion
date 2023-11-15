37ba2ed7929e302eecdbd946ed2a5610429d402e
jurecapuder <capuder.j@gmail.com>
2023-11-15 16:52:51 +0100

    Creating icon picker component

---
+++ components/icon-picker.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import EmojiPicker, { Theme } from "emoji-picker-react";
+import { useTheme } from "next-themes";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from "@/components/ui/popover";
