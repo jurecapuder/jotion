cec8195b325eb3ce441048b944998fd7be5428df
jurecapuder <capuder.j@gmail.com>
2023-11-15 16:48:27 +0100

    Add empty div for spacing

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -37,6 +37,7 @@ const DocumentIdPage = ({
 
   return (
     <div className="pb-40">
+      <div className="h-[35vh]" />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
       </div>
