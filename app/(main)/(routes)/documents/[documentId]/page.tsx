f33afcb0ced58e8e082751cccc5b9686a3ca2cab
jurecapuder <capuder.j@gmail.com>
2023-11-15 16:44:43 +0100

    Add toolbar to DocumentId page.

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "convex/react";
 
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/components/toolbar";
 
 interface DocumentIdPageProps {
   params: {
@@ -37,7 +38,7 @@ const DocumentIdPage = ({
   return (
     <div className="pb-40">
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-        DocumentId
+        <Toolbar initialData={document} />
       </div>
     </div>
   );
