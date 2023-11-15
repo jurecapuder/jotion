8f08bd943b9c0245e54b9718d8dd6d89c9def6eb
jurecapuder <capuder.j@gmail.com>
2023-11-15 16:42:33 +0100

    Update DocumentIdPage component

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -35,8 +35,10 @@ const DocumentIdPage = ({
   }
 
   return (
-    <div>
-      DocumentIdPage
+    <div className="pb-40">
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        DocumentId
+      </div>
     </div>
   );
 }
