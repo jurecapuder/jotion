bd714d1a97591f968ef6b2f7a1305a07208fd7f0
jurecapuder <capuder.j@gmail.com>
2023-11-17 23:16:31 +0100

    Add dialog header to cover image modal

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -12,7 +12,13 @@ export const CoverImageModal = () => {
 
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
-  
+      <DialogContent>
+        <DialogHeader>
+          <h2 className="text-center text-lg font-semibold">
+            Cover Image
+          </h2>
+        </DialogHeader>
+      </DialogContent>
     </Dialog>
   )
 };
