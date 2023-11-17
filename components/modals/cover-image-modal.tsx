4374b85cc084ade1879875a1d9796d3a7a6137d6
jurecapuder <capuder.j@gmail.com>
2023-11-17 23:14:28 +0100

    Creating cover image modal

---
+++ components/modals/cover-image-modal.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader
+} from "@/components/ui/dialog";
+import { useCoverImage } from "@/hooks/use-cover-image";
+
+export const CoverImageModal = () => {
+  const coverImage = useCoverImage();
+
+  return (
+    <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
+  
+    </Dialog>
+  )
+};
