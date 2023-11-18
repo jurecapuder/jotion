1de61c469379734eb4098139e0ab51d2599c7136
jurecapuder <capuder.j@gmail.com>
2023-11-18 23:52:35 +0100

    Add file upload functionality to CoverImageModal

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import { useState } from "react";
 import {
   Dialog,
   DialogContent,
   DialogHeader
 } from "@/components/ui/dialog";
 import { useCoverImage } from "@/hooks/use-cover-image";
+import { SingleImageDropzone } from "@/components/single-image-dropzone";
+import { useEdgeStore } from "@/lib/edgestore";
 
 export const CoverImageModal = () => {
+  const [file, setFile] = useState<File>();
+  const [ isSubmitting, setIsSubmitting ] = useState(false);
   const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
 
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
