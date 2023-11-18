8652714bbda9fc4070a54897639807ff5dfdc30c
jurecapuder <capuder.j@gmail.com>
2023-11-18 23:47:22 +0100

    Update SingleImageDropzone component UI

--- components/single-image-dropzone.tsx
+++ components/single-image-dropzone.tsx
@@ -152,9 +152,8 @@ const SingleImageDropzone = React.forwardRef<HTMLInputElement, InputProps>(
             // Upload Icon
             <div className="flex flex-col items-center justify-center text-xs text-gray-400">
               <UploadCloudIcon className="mb-2 h-7 w-7" />
-              <div className="text-gray-400">drag & drop to upload</div>
-              <div className="mt-3">
-                <Button disabled={disabled}>select</Button>
+              <div className="text-gray-400">
+                Click or drag file to this area to upload
               </div>
             </div>
           )}
