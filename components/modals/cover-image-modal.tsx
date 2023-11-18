04313245588c3810b20f76d8f50f41582a128bbb
jurecapuder <capuder.j@gmail.com>
2023-11-18 23:57:24 +0100

    Add file upload functionality to CoverImageModal

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useState } from "react";
+import { useMutation } from "convex/react";
+import { useParams } from "next/navigation";
+
 import {
   Dialog,
   DialogContent,
@@ -9,12 +12,33 @@ import {
 import { useCoverImage } from "@/hooks/use-cover-image";
 import { SingleImageDropzone } from "@/components/single-image-dropzone";
 import { useEdgeStore } from "@/lib/edgestore";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
 
 export const CoverImageModal = () => {
-  const [file, setFile] = useState<File>();
-  const [ isSubmitting, setIsSubmitting ] = useState(false);
+  const params = useParams();
+  const update = useMutation(api.documents.update);
   const coverImage = useCoverImage();
   const { edgestore } = useEdgeStore();
+
+  const [file, setFile] = useState<File>();
+  const [ isSubmitting, setIsSubmitting ] = useState(false);
+
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+      
+      const res = await edgestore.publicFiles.upload({
+        file
+      });
+
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url
+      });
+    }
+  };
 
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
