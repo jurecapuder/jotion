8a2e14267a0e4af840159f7bd59f7fa058e6c63e
jurecapuder <capuder.j@gmail.com>
2023-11-19 19:26:22 +0100

    Refactor cover component and add image buttons

--- components/cover.tsx
+++ components/cover.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-
 import Image from "next/image";
+import { ImageIcon, X } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 interface CoverImageProps {
   url?: string;
@@ -26,6 +28,32 @@ export const Cover = ({
           alt="Cover"
           className="object-cover"
         />
+      )}
+
+      {url && !preview && (
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+          <Button
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+
+            Change cover
+          </Button>
+
+          <Button
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <X className="h-4 w-4 mr-2" />
+
+            Remove
+          </Button>
+        </div>
       )}
     </div>
   )
