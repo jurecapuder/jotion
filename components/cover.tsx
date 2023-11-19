3213c41dd0a007d8cdde605022df6f524039131c
jurecapuder <capuder.j@gmail.com>
2023-11-19 19:30:45 +0100

    Add useMutation and useCoverImage hooks to Cover component

--- components/cover.tsx
+++ components/cover.tsx
@@ -2,9 +2,15 @@
 
 import Image from "next/image";
 import { ImageIcon, X } from "lucide-react";
+import { useMutation } from "convex/react";
+import { useParams } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { useCoverImage } from "@/hooks/use-cover-image";
+import { api } from "@/convex/_generated/api";
+import { remove } from "@/convex/documents";
+import { Id } from "@/convex/_generated/dataModel";
 
 interface CoverImageProps {
   url?: string;
@@ -15,6 +21,16 @@ export const Cover = ({
   url,
   preview
 }: CoverImageProps) => {
+  const params = useParams();
+  const coverImage = useCoverImage();
+  const removeCoverImage = useMutation(api.documents.removeCoverImage);
+
+  const onRemove = () => {
+    removeCoverImage({
+      id: params.documentId as Id<"documents">
+    });
+  };
+
   return (
     <div className={cn(
       "relative w-full h-[35vh] group",
@@ -33,7 +49,7 @@ export const Cover = ({
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
-            onClick={() => {}}
+            onClick={coverImage.onOpen}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
@@ -44,7 +60,7 @@ export const Cover = ({
           </Button>
 
           <Button
-            onClick={() => {}}
+            onClick={onRemove}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
