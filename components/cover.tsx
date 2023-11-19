3f341cd5ce14e2fc751a43d7736cd6a8248cbca6
jurecapuder <capuder.j@gmail.com>
2023-11-19 19:20:01 +0100

    Add cover image to the Cover component

--- components/cover.tsx
+++ components/cover.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+
+import Image from "next/image";
+
 interface CoverImageProps {
   url?: string;
   preview?: boolean;
@@ -10,8 +14,19 @@ export const Cover = ({
   preview
 }: CoverImageProps) => {
   return (
-    <div>
-      Cover
+    <div className={cn(
+      "relative w-full h-[35vh] group",
+      !url && "h-[12vh]",
+      url && "bg-muted"
+    )}>
+      {!!url && (
+        <Image
+          src={url}
+          fill
+          alt="Cover"
+          className="object-cover"
+        />
+      )}
     </div>
   )
 };
