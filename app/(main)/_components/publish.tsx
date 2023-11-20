227442bd09f9ba268848687085c11f8d9f0e3fe2
jurecapuder <capuder.j@gmail.com>
2023-11-20 18:11:24 +0100

    Add publish functionality to note component

--- app/(main)/_components/publish.tsx
+++ app/(main)/_components/publish.tsx
@@ -84,6 +84,42 @@ export const Publish = ({
           )}
         </Button>
       </PopoverTrigger>
+
+      <PopoverContent
+        className="w-72"
+        align="end"
+        alignOffset={8}
+        forceMount
+      >
+        {initialData.isPublished ? (
+          <div>
+            Published
+          </div>
+        ) : (
+          <div className="flex flex-col items-center justify-center">
+            <Globe
+              className="h-8 w-8 text-muted-foreground mb-2"
+            />
+
+            <p className="text-sm font-medium mb-2">
+              Publish this note
+            </p>
+
+            <span className="text-xs text-muted-foreground mb-4">
+              Share your work with others.
+            </span>
+
+            <Button
+              disabled={isSubmitting}
+              onClick={onPublish}
+              className="w-full text-xs"
+              size="sm"
+            >
+              Publish
+            </Button>
+          </div>
+        )}
+      </PopoverContent>
     </Popover>
   );
 }
