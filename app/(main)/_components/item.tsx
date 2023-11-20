c7c72a936f08c693780177668a4db1bf4d727747
jurecapuder <capuder.j@gmail.com>
2023-11-20 13:36:29 +0100

    Fix archive function and add redirect after archiving

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -65,7 +65,8 @@ export const Item = ({
 
     if (!id) return;
 
-    const promise = archive({ id });
+    const promise = archive({ id })
+      .then(() => router.push("/documents"));
 
     toast.promise(promise, {
       loading: "Moving to trash...",
@@ -91,7 +92,7 @@ export const Item = ({
         if (!expanded) {
           onExpand?.();
         }
-        //router.push(`/documents/${documentId}`);
+        router.push(`/documents/${documentId}`);
       });
 
     toast.promise(promise, {
