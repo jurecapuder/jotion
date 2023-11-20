4bbdba31b0854045a508c46fdbd20996a55761b0
jurecapuder <capuder.j@gmail.com>
2023-11-20 13:11:38 +0100

    Add file upload functionality to Editor component

--- components/editor.tsx
+++ components/editor.tsx
@@ -11,6 +11,8 @@ import {
 } from "@blocknote/react";
 import "@blocknote/core/style.css"
 
+import { useEdgeStore } from "@/lib/edgestore";
+
 interface EditorProps {
   onChange: (value: string) => void;
   initialContent?: string;
@@ -23,6 +25,15 @@ export const Editor = ({
   editable
 }: EditorProps) => {
   const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File) => {
+    const response = await edgestore.publicFiles.upload({
+      file
+    });
+
+    return response.url;
+  }
 
   const editor: BlockNoteEditor = useBlockNote({
     editable,
@@ -33,6 +44,7 @@ export const Editor = ({
     onEditorContentChange: (editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
     },
+    uploadFile: handleUpload
   });
 
   return (
