a8878a56953ee1cd585820c2e5121089eb0de820
jurecapuder <capuder.j@gmail.com>
2023-11-20 13:41:40 +0100

    Refactor Editor component

--- components/editor.tsx
+++ components/editor.tsx
@@ -19,7 +19,7 @@ interface EditorProps {
   editable?: boolean;
 };
 
-export const Editor = ({
+const Editor = ({
   onChange,
   initialContent,
   editable
@@ -56,3 +56,5 @@ export const Editor = ({
     </div>
   )
 }
+
+export default Editor;
