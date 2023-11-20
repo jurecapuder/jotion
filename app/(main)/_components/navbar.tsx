66e428b3006b3b5191d4b27fbf2ff94cecd35844
jurecapuder <capuder.j@gmail.com>
2023-11-20 17:43:24 +0100

    Add Publish component to navbar

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -10,6 +10,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { Title } from "./title";
 import { Banner } from "./banner";
 import { Menu } from "./menu";
+import { Publish } from "./publish";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -57,6 +58,8 @@ export const Navbar = ({
           <Title initialData={document} />
 
           <div className="flex items-center gap-x-2">
+            <Publish initialData={document} />
+
             <Menu documentId={document._id} />
           </div>
         </div>
