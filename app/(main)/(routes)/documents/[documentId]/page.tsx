4e6b4906fd1735289469304fb0116d4d12289c6c
jurecapuder <capuder.j@gmail.com>
2023-11-20 12:46:22 +0100

    Add Editor component to DocumentIdPage

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -7,6 +7,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Editor } from "@/components/editor";
 
 interface DocumentIdPageProps {
   params: {
@@ -57,6 +58,11 @@ const DocumentIdPage = ({
 
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
+
+        <Editor
+          onChange={() => {}}
+          initialContent={document.content}
+        />
       </div>
     </div>
   );
