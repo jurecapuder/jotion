36850a91b8e75aa79e42e4288c6735e9030487c4
jurecapuder <capuder.j@gmail.com>
2023-11-20 18:03:28 +0100

    Add publish button with globe icon

--- app/(main)/_components/publish.tsx
+++ app/(main)/_components/publish.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import { useMutation } from "convex/react";
+import { toast } from "sonner";
+import { Globe } from "lucide-react";
 
 import { Doc } from "@/convex/_generated/dataModel";
 import {
@@ -11,7 +13,7 @@ import {
 } from "@/components/ui/popover";
 import { useOrigin } from "@/hooks/use-origin";
 import { api } from "@/convex/_generated/api";
-import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
 
 interface PublishProps {
   initialData: Doc<"documents">;
@@ -70,8 +72,18 @@ export const Publish = ({
   };
 
   return (
-    <div>
-      Publish
-    </div>
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button size="sm" variant="ghost">
+          Publish
+
+          {initialData.isPublished && (
+            <Globe
+              className="text-sky-500 w-4 h-4 ml-2"
+            />
+          )}
+        </Button>
+      </PopoverTrigger>
+    </Popover>
   );
 }
